dc9002ccba9a74652a2ba1af7351e54714276480
Suresh Alse <alseambusher@gmail.com>
2013-05-17 19:48:41 +0530

    bug fixes

--- config.php
+++ config.php
@@ -1,16 +1,14 @@
 <?php  if ( ! defined("BASEPATH")) exit("No direct script access allowed");
-					$wikiapp_name="Anim&eacute; Wiki";
-					$wikiapp_description="Cricket is a bat-and-ball game played between two teams of 11 players on a field, at the centre of which is a rectangular 22-yard long pitch. One team bats, trying to score as many runs as possible while the other team bowls and fields, trying to dismiss the batsmen and thus limit the runs scored by the batting team. A run is scored by the striking batsman hitting the ball with his bat, running to the opposite end of the pitch and touching the crease there without being dismissed. The teams switch between batting and fielding at the end of an innings.
-In professional cricket the length of a game ranges from 20 overs of six bowling deliveries per side to Test cricket played over five days. The Laws of Cricket are maintained by the International Cricket Council (ICC) and the Marylebone Cricket Club (MCC) with additional Standard Playing Conditions for Test matches and One Day Internationals.[1]
-Cricket was first played in southern England in the 16th century. By the end of the 18th century, it had developed into the national sport of England. The expansion of the British Empire led to cricket being played overseas and by the mid-19th century the first international matches were being held. The ICC, the game's governing body, has ten full members.[2] The game is played particularly in Australasia, the Indian subcontinent, the West Indies, Southern Africa and England.";
-					$db_host="localhost";
-					$db_username="root";
-					$db_password="alse";
-					$db_name="indibits_wiki";
-					$admin_username="alse";
-					$admin_password="e94f542bb18a46b8dac6dc2c1c300b5e";
-					$auto=array("session");
-					$copyright="none";
-					$terms="We are not responsible for anything done using this wiki application";
+					$wikiapp_name="";
+					$wikiapp_description="";
+					$db_host="";
+					$db_username="";
+					$db_password="";
+					$db_name="";
+					$admin_username="";
+					$admin_password="";
+					$auto=array("session","database");
+					$copyright="";
+					$terms="";
 					$default_controller="install";
 					$theme="bootstrap";?>
