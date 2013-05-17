26a56ef62299c53518ac427951ff1512e10ba085
Suresh Alse <alseambusher@gmail.com>
2013-05-17 19:55:03 +0530

    deployed app

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php  if ( ! defined("BASEPATH")) exit("No direct script access allowed");
 					$wikiapp_name="";
 					$wikiapp_description="";
-					$db_host="";
-					$db_username="";
-					$db_password="";
-					$db_name="";
+					$db_host="localhost";
+					$db_username="root";
+					$db_password="alse";
+					$db_name="indibits_wiki";
 					$admin_username="";
 					$admin_password="";
 					$auto=array("session","database");
